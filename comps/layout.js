05556cb0220958d3068c51c580544cd43e9b329d
gugushall <107054293+gugushall@users.noreply.github.com>
2023-01-25 12:48:01 +0200

    tailwind3

--- comps/layout.js
+++ comps/layout.js
@@ -3,7 +3,7 @@ import Navbar from "comps/navbar.js"
 
 const Layout = ({ children }) => {
   return (
-    <div className="   max-w-screen-lg mx-auto my-0    ">
+    <div className="   max-w-4xl mx-auto my-0    ">
       <Navbar />
       { children }
       <Footer />
